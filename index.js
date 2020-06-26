0435219d458c3f710bd53cb950c2c527200ae789
Nitish09mishra <42340130+Nitish09mishra@users.noreply.github.com>
2020-06-26 18:53:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 
-const currentDate= new Date();
-const year= currentDate.getFullYear();
-
+const currentDate= new Date()
+const year= currentDate.getFullYear()
+const time= currentDate.getHours()
+console.log(time)
 $(".year").text(year);
+
+if (time < 12) {
+  $(".greeting").text("Good morning!  ")
+  $(".greeting").css("color", "green")
+} 
+else if (time >= 12 && time <= 16) {
+  $(".greeting").text("Good afternoon!  ")
+  $(".greeting").css("color", "red")
+} 
+else {
+  $(".greeting").text("Good evening!  ")
+  $(".greeting").css("color", "rgb(13,17,85)")
+}
